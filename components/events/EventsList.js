fcbe7fb92df60bbfc0e088ca2e29c7d8c000ebb1
Bhairesh M <bhairesh@arithaconsulting.com>
2022-04-26 10:39:47 +0530

    swr installed.

--- components/events/EventsList.js
+++ components/events/EventsList.js
@@ -2,7 +2,7 @@ import EventItem from "./EventItem";
 import classes from "./eventList.module.css";
 
 function EventsList(props) {
-  if (props.events.length <= 0) return <div style={{ "text-align": "center" }}>No data found!!</div>;
+  if (props.events.length <= 0) return <div style={{ textAlign: "center" }}>No data found!!</div>;
 
   return (
     <ul className={classes.list}>
